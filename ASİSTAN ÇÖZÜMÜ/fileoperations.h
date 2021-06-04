96675eef3be062e3aa8a9853ce912831784bd835
Barış Beydemir <56716335+ituitis-beydemir18@users.noreply.github.com>
2021-06-04 09:18:14 +0300

    Add files via upload

---
+++ ASİSTAN ÇÖZÜMÜ/fileoperations.h
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#ifndef DOSYAISLEMLERI_H
+#define DOSYAISLEMLERI_H
+#include <stdio.h>
+#include "record.h"
+
+struct Dosya
+{
+	char *dosyaadi;
+	FILE *teldefteri;
+	void olustur();
+	void kapat();
+	void ekle(Tel_Kayit *);
+	int ara(char[]);
+	void sil(int kayitno);
+	void guncelle(int kayitno, Tel_Kayit *);
+};
+#endif
